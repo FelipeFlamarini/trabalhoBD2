6f5800ea7e45badc6676dc1a067b48ff54042baf
FelipeFlamarini <felipeflamarini@hotmail.com>
2023-11-15 12:45:35 -0300

    base de todas as tables

--- receitas.sql
+++ receitas.sql
@@ -1,15 +1,70 @@
--- \set QUIET ON
-DROP TABLE IF EXISTS estado;
+DROP TABLE IF EXISTS receitas;
+
+DROP TABLE IF EXISTS ingredientes;
+
+DROP TABLE IF EXISTS receitas_ingredientes;
+
+DROP TABLE IF EXISTS usuarios;
+
+DROP TABLE IF EXISTS estados;
+
+DROP TABLE IF EXISTS caracteristicas;
 
 CREATE TABLE
-    estado (
+    receitas (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        caracteristicas int NOT NULL,
+        FOREIGN KEY (caracteristicas) REFERENCES caracteristicas (id) estado INT NOT NULL,
+        FOREIGN KEY (estado) REFERENCES estados (id),
+        criadoPor int NOT NULL,
+        FOREIGN KEY (criadoPor) REFERENCES usuarios (id),
+    );
+
+CREATE TABLE
+    ingredientes (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+    );
+
+CREATE TABLE
+    receitas_ingredientes (
+        id SERIAL PRIMARY KEY,
+        receita INT NOT NULL,
+        FOREIGN KEY (receita) REFERENCES receitas (id),
+        ingrediente INT NOT NULL,
+        FOREIGN KEY (ingrediente) REFERENCES ingredientes (id),
+    );
+
+CREATE TABLE
+    usuarios (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        senha VARCHAR(255) NOT NULL,
+        estado INT NOT NULL,
+        FOREIGN KEY (estado) REFERENCES estados (id),
+    );
+
+CREATE TABLE
+    estados (
         id SERIAL PRIMARY KEY,
         nome VARCHAR(255) NOT NULL,
         sigla VARCHAR(2) NOT NULL
     );
 
 INSERT INTO
-    estado (nome, sigla)
+    TABLE ingredientes (nome)
+VALUES
+    ('frango') ('carne') ('queijo') ('presunto') ('ovo') ('alface') ('tomate') ('cebola') ('picles') ('pepino') ('molho especial') ('ketchup') ('mostarda') ('maionese') ('pão com gergelim') ('pão sem gergelim') ('pão de forma') ('pão de hambúrguer') ('pão de cachorro quente') ('pão de queijo') ('pão de batata') ('pão de alho') ('pão de milho') ('pão de centeio') ('pão de aveia') ('pão de trigo') ('pão de cevada');
+
+INSERT INTO
+    usuarios (nome, email, senha, estado)
+VALUES
+    ('João', 'joao@gmail.com', '123456', 1);
+
+INSERT INTO
+    estados (nome, sigla)
 VALUES
     ('São Paulo', 'SP'),
     ('Rio de Janeiro', 'RJ'),
@@ -38,3 +93,35 @@ VALUES
     ('Acre', 'AC'),
     ('Amapá', 'AP'),
     ('Roraima', 'RR');
+
+CREATE TABLE
+    caracteristicas (
+        id SERIAL PRIMARY FOREIGN KEY,
+        nome VARCHAR(255) NOT NULL,
+        éDoce BOOLEAN,
+        éSalgado BOOLEAN,
+        éÁcido BOOLEAN,
+        éAmargo BOOLEAN,
+        éPicante BOOLEAN,
+        éAzedo BOOLEAN,
+        éFrio BOOLEAN,
+        éQuente BOOLEAN,
+        éMorno BOOLEAN,
+        éCrocante BOOLEAN,
+        éMacio BOOLEAN,
+        éLíquido BOOLEAN,
+        éPastoso BOOLEAN,
+        éSólido BOOLEAN,
+        éGorduroso BOOLEAN,
+        éFibroso BOOLEAN,
+        éFresco BOOLEAN,
+        éFermentado BOOLEAN,
+        paraNatal BOOLEAN,
+        paraAnoNovo BOOLEAN,
+        paraCarnaval BOOLEAN,
+        paraPáscoa BOOLEAN,
+        paraFestaJunina BOOLEAN,
+        paraDiaDasCrianças BOOLEAN,
+        paraHalloween BOOLEAN,
+        paraNatal BOOLEAN,
+    );
