8bb0f7445fd57672d3d8528de5afc7bb775cdf46
FelipeFlamarini <felipeflamarini@hotmail.com>
2023-11-17 10:56:06 -0300

    incrementando caracteristicas

--- base.sql
+++ base.sql
@@ -23,7 +23,7 @@ CREATE TABLE
         nome VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
         senha VARCHAR(255) NOT NULL,
-        estado_id INT NOT NULL references estados (id)
+        estado_sigla VARCHAR(2) NOT NULL REFERENCES estados (sigla)
     );
 
 CREATE TABLE
@@ -74,7 +74,8 @@ CREATE TABLE
         paraFestaJunina BOOLEAN DEFAULT FALSE,
         paraDiaDasCrianças BOOLEAN DEFAULT FALSE,
         paraHalloween BOOLEAN DEFAULT FALSE,
-        paraAlmoço BOOLEAN DEFAULT FALSE
+        paraAlmoço BOOLEAN DEFAULT FALSE,
+        paraJantar BOOLEAN DEFAULT FALSE
     );
 
 INSERT INTO
